54dc16a98645db790864eb865a13aa14254060e0
moonstrider20 <46873254+moonstrider20@users.noreply.github.com>
2021-09-01 15:21:25 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,16 +8,36 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            //Test player input
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            Console.WriteLine(command);
+            Commands command = Commands.UNKNOWN;
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch(command)
+                {
+                    case Commands.LOOK:
+                        outputString = ("A rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        outputString = $"You moved {command}.";
+                        break;
+                    case Commands.QUIT:
+                        outputString = ("Thank you for playing!");
+                        break;
+                    default:
+                        outputString = ("Unknown command.");
+                        break;
+                }
+
+                Console.WriteLine(outputString);
+            }
         }
 
-        //Test enum Commands
-        private static Commands ToCommand(string commandString)
-        {
-            return Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
-        }
+        private static Commands ToCommand(string commandString) => (Enum.TryParse<Commands>(commandString, true, out Commands result) ? result : Commands.UNKNOWN);
     }
 }
